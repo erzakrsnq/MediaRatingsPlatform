030d84bd814d6f4d5b007553cec52cac2a6e6bf9
erzakrsnq <if23b304@technikum-wien.at>
2025-09-08 14:12:40 +0200

    Start Projekt

---
+++ src/main/java/org/example/server/http/Request.java
@@ -0,0 +1,11 @@
+package org.example.server.http;
+
+public class Request {
+    private String method;
+
+    private String path;
+
+    public String getMethod() {
+        return method;
+    }
+}
