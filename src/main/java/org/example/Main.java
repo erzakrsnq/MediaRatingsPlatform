93e7bfef5b2297a4564f2a1dbed9ce06f6cc6ba8
erzakrsnq <if23b304@technikum-wien.at>
2025-09-17 10:34:23 +0200

    add basic HTTP server and echo endpoint

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,15 @@
 package org.example;
 
+import org.example.server.http.HttpServerApp;
+
 public class Main {
     public static void main(String[] args) {
+        HttpServerApp server = new HttpServerApp(8080);
+        server.start();
+    }
+}
 
-        // Server server = new Server(
+// Server server = new Server(
         //  8080,
         //  new EchoApplication()
         // )
-        // server.start();
-    }
-}
--- src/main/java/org/example/server/http/Response.java
+++ src/main/java/org/example/server/http/Response.java
@@ -1,4 +1,25 @@
 package org.example.server.http;
 
 public class Response {
+    private final int statusCode;
+    private final String contentType;
+    private final String body;
+
+    public Response() {
+        this.statusCode = 200;
+        this.contentType = "application/json";
+        this.body = "";
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public String getBody() {
+        return body;
+    }
 }
